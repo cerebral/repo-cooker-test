4c35beba44c51efdd15606d002258e966e89cbae
Gaspard Bucher <gaspard@lucidogen.io>
2017-08-10 14:44:54 +0200

    feat(repo-cooker-test): auto publish using travis

--- scripts/cooker.js
+++ scripts/cooker.js
@@ -1,14 +1,15 @@
 import { Cooker } from 'repo-cooker'
 
+const dryRun = process.argv[2] !== '--publish'
+process.env.REPO_COOKER_GITHUB_TOKEN = process.env.GH_TOKEN
+
 export const cooker = Cooker({
-  devtools: {
-    host: 'localhost:8787',
-  },
+  devtools: null,
   path: '.',
   packagesGlobs: [
     'packages/node_modules/*',
     'packages/node_modules/@repo-cooker-test/*',
     '!packages/node_modules/@repo-cooker-test',
   ],
-  dryRun: true,
+  dryRun,
 })
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,7 +1,9 @@
+// tree to run with `repo-cooker publish`. extra arguments could be parsed and putted in props
 import { cooker } from './cooker'
 import * as cook from 'repo-cooker/actions'
+import releaseNotesTemplate from './releaseNotesTemplate'
 
-cooker.run('publish', [
+cooker.cook('publish', [
   cook.getLatestReleaseHash,
   cook.getHistoryFromHash,
   cook.getRawCommitsFromHistory,
@@ -11,13 +13,33 @@ cooker.run('publish', [
   cook.relatedPackagesByPackage,
   cook.getCurrentVersionByPackage,
   cook.evaluateNewVersionByPackage,
+  cook.byBranch,
+  {
+    next: cook.remap(
+      'newVersionByPackage',
+      version => `${version}-${Date.now()}`
+    ),
+    otherwise: [],
+  },
   cook.writeVersionsToPackages,
+  cook.runNpmScript('prepublish'),
   cook.publishUnderTemporaryNpmTag,
-  cook.mapTemporaryNpmTagToLatest,
+  cook.byBranch,
+  {
+    master: cook.mapTemporaryNpmTagTo('latest'),
+    next: cook.mapTemporaryNpmTagTo('next'),
+    otherwise: [],
+  },
   cook.resetRepository,
-  cook.tagCurrentCommit,
-  cook.pushTagToRemote,
-  cook.createReleaseNotes(release => `some release notes`),
-  cook.createGithubRelease,
+  cook.byBranch,
+  {
+    master: [
+      cook.tagCurrentCommit,
+      cook.pushTagToRemote,
+      cook.createReleaseNotes(releaseNotesTemplate),
+      cook.createGithubRelease,
+    ],
+    otherwise: [],
+  },
   cook.fireworks,
 ])
