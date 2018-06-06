1c9eec44e16b88a04809c115d4713b142901e049
Gaspard Bucher <gaspard@lucidogen.io>
2018-06-06 17:58:30 +0200

    fix(repo-cooker-test): using default release in repo-cooker

--- scripts/link.js
+++
@@ -1,5 +0,0 @@
-// import {sequence, parallel} from 'function-tree'
-// import {} from 'repo-cooker'
-
-// tree to run with `repo-cooker link`. extra arguments could be parsed and putted in props
-// export []
